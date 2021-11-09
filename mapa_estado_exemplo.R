8861cd659abcf61eacbb4093849480cdd393b690
marcelo.gadelha <marcelo.gadelha@cidadania.gov.br>
2021-11-09 20:54:05 -0300

    continuando implementando...

--- mapa_estado_exemplo.R
+++ mapa_estado_exemplo.R
@@ -1,7 +1,12 @@
+#install.packages("rdal")
+#install.packages("leaflet")
 
 library(rgdal) 
+library(leaflet)
 
 shp <- readOGR("shapes\\estados\\.", "BR_UF_2020", stringsAsFactors=FALSE, encoding="UTF-8")
 dados <- read.csv(".\\geo\\estados.csv", encoding = "UTF-8")
+score <- 1:27
+dados <- cbind(dados, score)
 
-
+brasileiropg <- merge(shp,dados, by.x = "ID", by.y = "CODIGO")
