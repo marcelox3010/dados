bca5ca6222f634fffbc56866e08323ee5750a7a0
Marcelo G Gadelha <marcelox3010@gmail.com>
2021-11-09 22:15:42 -0300

    exemplo mapa estado.

--- mapa_estado_exemplo.R
+++ mapa_estado_exemplo.R
@@ -1,5 +1,3 @@
-#install.packages("rdal")
-#install.packages("leaflet")
 
 library(rgdal) 
 library(leaflet)
@@ -9,4 +7,31 @@ dados <- read.csv(".\\geo\\estados.csv", encoding = "UTF-8")
 score <- 1:27
 dados <- cbind(dados, score)
 
-brasileiropg <- merge(shp,dados, by.x = "ID", by.y = "CODIGO")
+brasileiropg <- merge(shp,dados, by.x = "CD_UF", by.y = "CODIGO")
+
+proj4string(brasileiropg) <- CRS("+proj=longlat +datum=WGS84 +no_defs")
+
+Encoding(brasileiropg$NM_UF) <- "UTF-8"
+
+brasileiropg$Score[is.na(brasileiropg$score)] <- 0
+
+pal <- colorBin("Blues",domain = NULL,n=5) #cores do mapa
+
+state_popup <- paste0("<strong>Estado: </strong>", 
+                      brasileiropg$NM_UF, 
+                      "<br><strong>Pontos: </strong>", 
+                      brasileiropg$score)
+
+leaflet(data = brasileiropg) %>%
+  
+  addProviderTiles("CartoDB.Positron") %>%
+  
+  addPolygons(fillColor = ~pal(brasileiropg$score), 
+              fillOpacity = 0.8, 
+              color = "#BDBDC3", 
+              weight = 1, 
+              popup = state_popup) %>%
+  
+  addLegend("bottomright", pal = pal, values = ~brasileiropg$score,
+            title = "Pontos Conquistados",
+            opacity = 1)
